9cd9cf47ebcb77f68286809c0982824ad117c729
IsmaelVarela <31021110+IsmaelVarela@users.noreply.github.com>
2018-03-12 13:37:48 -0700

    controllers insert
    centro salud,datos personales

--- CADE/app/Datos_personales.php
+++ CADE/app/Datos_personales.php
@@ -9,6 +9,7 @@ class Datos_personales extends Model
   protected $primaryKey='Id';
   protected $table='Datos_personales';
   protected $fillable=[
-  'Nombre','Apellido_p','Apellido_m','Fecha_nacimiento','Lugar_nacimiento','Edad','Sexo','Edo_civil','Tipo_sangre'.'Peso','Estatura','Talla','Calzado','Domicilio','Cp','Localida','Tel'
+  'Nombre','Apellido_p','Apellido_m','Fecha_nacimiento','Lugar_nacimiento','Edad',
+  'Sexo','Edo_civil','Tipo_sangre','Estatura','Talla','Calzado','Domicilio','Cp','Localida','Tel'
   ];
 }
--- CADE/routes/web.php
+++ CADE/routes/web.php
@@ -31,10 +31,12 @@ Route::group(['prefix'=>'admin', 'as'=>'admin.'],function(){
   Route::get('/',function(){return "yeah";});
   Route::get('/usuarios','usuarioscontroller@index')->name('usuarios');
   Route::get('/archivos','archivoscontroller@index')->name('archivos');
-
+  Route::get('/centrosalud','Centro_saludcontroller@index')->name('centrosalud');
+  Route::get('/datospersonales','datospersonalescontroller@index')->name('datospersonales');
   Route::resource('usuarios','usuarioscontroller');
   Route::resource('archivos','archivoscontroller');
-
+  Route::resource('centrosalud','Centro_Saludcontroller');
+  Route::resource('datospersonales','datospersonalescontroller');
 });
 
 Auth::routes();
