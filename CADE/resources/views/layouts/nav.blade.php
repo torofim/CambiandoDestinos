405128604d3c6ac5be5fc9098610312492c358a8
Nokturne <krothaloinf@gmail.com>
2018-04-17 16:23:49 -0600

    naveado

--- CADE/resources/views/layouts/nav.blade.php
+++ CADE/resources/views/layouts/nav.blade.php
@@ -1,43 +1,40 @@
-<nav class="navbar navbar-inverse">
- <div class="container-fluid">
+<nav class="navbar navbar-inverse sidebar" role="navigation">
+  <div class="container-fluid">
+  <!-- Brand and toggle get grouped for better mobile display -->
   <div class="navbar-header">
-   <button type="button" class="collapsed navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-9" aria-expanded="false">
-    <span class="sr-only">Toggle navigation</span>
-     <span class="icon-bar"></span>
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-sidebar-navbar-collapse-1">
+      <span class="sr-only">Toggle navigation</span>
       <span class="icon-bar"></span>
-       <span class="icon-bar"></span>
-        </button>
-         <a href="/principal" class="navbar-brand"> Fundación Cambiando Destinos</a>
-          </div>
-           <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-9">
-            <ul class="nav navbar-nav">
-             <li class="">
-               <a href="/admin/dash">Inicio</a>
-             </li>
-             <li>
-               <a href="/admin/benef">Beneficiarios</a>
-             </li>
-              <li>
-                <a href="/admin/registrados">Registros</a>
-              </li>
-              <li>
-                <a href="/admin/graficos">Graficos</a>
-              </li>
-              <div class="navspace">
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <a class="navbar-brand" href="#">Cambiando Destinos</a>
+  </div>
+  <!-- Collect the nav links, forms, and other content for toggling -->
+  <div class="collapse navbar-collapse" id="bs-sidebar-navbar-collapse-1">
+    <ul class="nav navbar-nav">
+      </li>
+      <li><a href="#">Inicio<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-home"></span></a></li>
+      <li ><a href="#">Usuarios<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-user"></span></a></li>
+      <li ><a href="#">Beneficiarios<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-leaf"></span></a></li>
+        <li ><a href="#">Encuestas<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-list-alt"></span></a></li>
+      <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Settings <span class="caret"></span><span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-cog"></span></a>
+        <ul class="dropdown-menu forAnimate" role="menu">
+          <li>
+            <a class="dropdown-item" href="{{ route('logout') }}"
+                 onclick="event.preventDefault();
+                               document.getElementById('logout-form').submit();" >
+                  Cerrar sesion
+              </a>
 
-              </div>
-              <li>
-                <a class="dropdown-item" href="{{ route('logout') }}"
-                   onclick="event.preventDefault();
-                                 document.getElementById('logout-form').submit();" >
-                    Cerrar sesion
-                </a>
-
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                    @csrf
-                </form>
-              </li>
-              </ul>
-               </div>
-                </div>
-                 </nav>
+              <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                  @csrf
+              </form>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</div>
+</nav>
