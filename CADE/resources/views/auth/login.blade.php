ea62e1a4a8ac45d8f86497a0e9e0caca4d83e111
torofim <eduardo.fimbres21@gmail.com>
2018-04-24 17:54:01 -0600

    ffff

--- CADE/resources/views/auth/login.blade.php
+++ CADE/resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container" style="margin-left:25%;margin-top:2%;" >
+<div class="container" style="margin-left:25%;margin-top:8%;" >
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
--- CADE/resources/views/layouts/app.blade.php
+++ CADE/resources/views/layouts/app.blade.php
@@ -17,12 +17,9 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+              <h1 style="font-size:50px;margin-left:18%;"><a href="/" style="text-decoration:none;color:gray;">Fundacion Cambiando Destinos</a></h1>
+
+
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
@@ -34,8 +31,7 @@
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
                         @guest
-                            <li><a class="nav-link" href="{{ route('login') }}">Login</a></li>
-                            <li><a class="nav-link" href="{{ route('register') }}">Register</a></li>
+
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
