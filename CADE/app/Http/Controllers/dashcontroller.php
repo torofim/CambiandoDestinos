7f04adb397d3138c01cd7e6707586960826a5046
Mauriciotorresnk <krothaloinf@gmail.com>
2018-04-24 23:32:09 -0600

    asa

--- CADE/app/Http/Controllers/dashcontroller.php
+++ CADE/app/Http/Controllers/dashcontroller.php
@@ -27,12 +27,14 @@ class dashcontroller extends Controller
   {
      $benef = DB::select('select COUNT(*) AS total from datos_personales');
      $usuarios = DB::select('select COUNT(*) AS usu from users');
-     $taller=DB::select('select COUNT(*) AS btaller from taller');
-     $disc=DB::select('select Count(*) AS disca from discapacidad');
+     
+
     #dd($benef);
+
   # NO FUNCIONA CON LOS 2 JUNTOS NECESITO PREGUNTAR AL ING COMO SE ARREGLA
-     return view('dashprincipal', ['benef' => $benef])
-     ->with('usuarios',$usuarios)->with('taller',$taller)->with('disc',$disc);
+       return view('dashprincipal', ['benef' => $benef])
+       ->with('usuarios',$usuarios);
+
        #return view('LEFTMENU',['usuarios' => $usuarios]);
        #->width('nombre','EQUIPOS DE FUTBOL');
        #si se hace esto y el js esta en el blade solo se imprime como php {{$nombre}}
--- CADE/resources/views/dashprincipal.blade.php
+++ CADE/resources/views/dashprincipal.blade.php
@@ -10,12 +10,13 @@
   <body>
   @include('layouts.nav')
   <div class="main">
-    <div class="panel panel-default" style="background-color:transparent;">
+    <div class="panel panel-default">
         <div class="panel-body cold-md-2" id="sepa">
+
       </div>
     </div>
     <div class="panel2 panel-default col-xs-12 ">
-      <div class="panel-body2" style="border-bottom:groove; background-color:transparent;">
+      <div class="panel-body2" style="border-bottom:groove;">
         Dashboard
       </div>
     </div>
@@ -43,35 +44,25 @@
               </p>
             </div>
             <div class="panel panel-default2 col-md-3  col-sm-4 col-xs-6  " style="background-color:transparent; height:15vh;" >
-              <p style="border-bottom:groove; border-bottom-width:.5px;">Beneficiarios Disc.</p>
-              <p style="font-size:3.5rem;">
-                @forelse ($disc as $d)
-                {{ $d->disca }}
-                @empty
-                @endforelse</p>
+              <p style="border-bottom:groove; border-bottom-width:.5px;">Visitantes</p>
+              <p style="font-size:3.5rem;">145</p>
             </div>
             <div class="panel panel-default2 col-md-3  col-sm-4 col-xs-6 " style="background-color:transparent; height:15vh;">
               <p style="border-bottom:groove; border-bottom-width:.5px;">Beneficiarios de taller</p>
               <p style="font-size:3.5rem;">
-                @forelse ($taller as $t)
-                {{ $t->btaller }}
-                @empty
-                @endforelse</p>
+              
+              </p>
             </div>
 		      </div>
 	     </div>
        <div class="container-fluid">
          <div class="row">
-           <div class="panel panel-default2 col-md-6  col-sm-8 col-xs-12 " style="background-color:white; height:auto;">
+           <div class="panel panel-default2 col-md-6  col-sm-8 col-xs-12 " style="background-color:white; height:350px;">
              <canvas id="bar-chart" width="800" height="450"></canvas>
            </div>
-           <div class="panel panel-default2 col-md-6  col-sm-8 col-xs-12 " style="background-color:white;height:auto;">
+           <div class="panel panel-default2 col-md-6  col-sm-8 col-xs-12 " style="background-color:white;height:350px;">
              <canvas id="bar-chart2" width="800" height="450"></canvas>
            </div>
-         </div>
-       </div>
-       <div class="panel panel-default" style="background-color:transparent;">
-           <div class="panel-body cold-md-2" id="sepa">
          </div>
        </div>
   </div>
