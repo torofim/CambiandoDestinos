397202615524dca5db6325d96983cc2c9da542a0
porfirio diaz <eduardo.fimbres21@gmail.com>
2018-05-02 16:53:27 -0600

    f

--- CADE/resources/views/Formubenefi.blade.php
+++ CADE/resources/views/Formubenefi.blade.php
@@ -26,17 +26,138 @@
 
           <button class="accordion">Datos Personales</button>
   <div class="panel1">
-    <div class="form-group">
-  <label for="usr">Name:</label>
-  <input type="text" class="form-control" id="usr">
-</div>
-    <button id="btnformas"></button>
+    <div class="card-header" style="margin-bottom:5%; font-size:3rem; margin-left:1.5%;">Datos Personales</div>
+
+    <div class="card-body">
+      @if($errors->any())
+        <div class="alert alert-warning alert-dismissable">
+          <ul>
+
+          @foreach($errors->all() as $error)
+            <li>{{$error}}</li>
+          @endforeach
+          </ul>
+        </div>
+      @endif
+      @if(session()->has('mensaje'))
+        <div class="alert alert-success">
+          {{session()->get('mensaje')}}
+        </div>
+      @endif
+      {{Form::open(array('url'=>'/admin/datospersonales','files'=>true))}}
+        <div class="input=-group col-md-4">
+          <label for="nombre">Nombre</label><br>
+          {{Form::text('nombre','',array('class'=>'form-control','placeholder'=>'Nombre') )}}
+        </div>
+        <div class="input=-group col-md-4">
+          <label for="nombre">Apellido Paterno</label><br>
+          {{Form::text('ap_p','',array('class'=>'form-control','placeholder'=>'Apellido paterno') )}}
+        </div>
+        <div class="input=-group col-md-4">
+          <label for="nombre">Apellido materno</label><br>
+          {{Form::text('ap_m','',array('class'=>'form-control','placeholder'=>'Apellido materno') )}}
+        </div>
+        <div class="input=-group col-md-4">
+          <label for="nombre">Fecha nacimiento </label><br>
+          {{Form::date('fecha_na','',array('class'=>'form-control','placeholder'=>'Fecha nacimiento') )}}
+        </div>
+        <div class="input=-group col-md-4">
+          <label for="nombre">Lugar nacimiento </label><br>
+          {{Form::text('lugar_na','',array('class'=>'form-control','placeholder'=>'Lugar nacimiento') )}}
+        </div>
+        <div class="input=-group col-md-4">
+          <label for="nombre">Edad </label><br>
+          {{Form::number('edad','',array('class'=>'form-control','placeholder'=>'Edad') )}}
+        </div>
+        <div class="input=-group col-md-4">
+          <label for="nombre">Sexo </label><br>
+          {{Form::text('sexo','',array('class'=>'form-control','placeholder'=>'M o F') )}}
+        </div>
+        <div class="input=-group col-md-4">
+          <label for="nombre">Estado civil </label><br>
+          {{Form::text('edo_civil','',array('class'=>'form-control','placeholder'=>'Estado civil') )}}
+        </div>
+        <div class="input=-group col-md-4">
+          <label for="nombre">Tipo sanguineo </label><br>
+          {{Form::text('tipo_sangre','',array('class'=>'form-control','placeholder'=>'Tipo sangre') )}}
+        </div>
+        <div class="input=-group col-md-4">
+          <label for="nombre">Peso </label><br>
+          {{Form::text('peso','',array('class'=>'form-control','placeholder'=>'Tipo peso') )}}
+        </div>
+        <div class="input=-group col-md-4">
+          <label for="nombre">Estatura </label><br>
+          {{Form::number('estatura','',array('class'=>'form-control','placeholder'=>'Ejemplo: 1.6') )}}
+        </div>
+        <div class="input=-group col-md-4">
+          <label for="nombre">Talla </label><br>
+          {{Form::number('talla','',array('class'=>'form-control','placeholder'=>'Talla') )}}
+        </div>
+        <div class="input=-group col-md-4">
+          <label for="nombre">Calzado </label><br>
+          {{Form::number('calzado','',array('class'=>'form-control','placeholder'=>'Calzado') )}}
+        </div>
+        <div class="input=-group col-md-4">
+          <label for="nombre">Domicilio </label><br>
+          {{Form::text('domicilio','',array('class'=>'form-control','placeholder'=>'Domicilio') )}}
+        </div>
+        <div class="input=-group col-md-4">
+          <label for="nombre">Codigo Postal </label><br>
+          {{Form::number('cp','',array('class'=>'form-control','placeholder'=>'Cp') )}}
+        </div>
+        <div class="input=-group col-md-4">
+          <label for="nombre">Localidad </label><br>
+          {{Form::text('localidad','',array('class'=>'form-control','placeholder'=>'Localidad') )}}
+        </div>
+        <div class="input=-group col-md-4">
+          <label for="nombre">Telefono </label><br>
+          {{Form::number('tel','',array('class'=>'form-control','placeholder'=>'Telefono') )}}
+        </div>
+        <div class="input-group col-md-12" style="margin-left:2%; padding-top:2%;">
+          {{Form::submit('Enviar',array('class'=>'btn btn-primary'))}}
+        </div>
+      {{Form::close()}}
+    </div>
+      </div>
+
   </div>
 
   <button class="accordion">Centro de Salud</button>
   <div class="panel1">
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, aboris nisi ut aliquip ex ea commodo consequat.</p>
-    <button type="button" name="button" id="btnformas"></button>
+    <div class="card-body">
+      @if($errors->any())
+        <div class="alert alert-warning alert-dismissable">
+          <ul>
+
+          @foreach($errors->all() as $error)
+            <li>{{$error}}</li>
+          @endforeach
+          </ul>
+        </div>
+      @endif
+      @if(session()->has('mensaje'))
+        <div class="alert alert-success">
+          {{session()->get('mensaje')}}
+        </div>
+      @endif
+      {{Form::open(array('url'=>'/admin/Centrosalud','files'=>true))}}
+        <div class="input=-group col-md-4">
+          <label for="nombre">Edad</label><br>
+          {{Form::number('edad','',array('class'=>'form-control','placeholder'=>'Edad') )}}
+        </div>
+        <div class="input=-group col-md-4">
+          <label for="nombre">Peso (Kg)</label><br>
+          {{Form::number('peso','',array('class'=>'form-control','placeholder'=>'Peso') )}}
+        </div>
+        <div class="input=-group col-md-4">
+          <label for="nombre">Presion Arterial</label><br>
+          {{Form::text('presion','',array('class'=>'form-control','placeholder'=>'Presion') )}}
+        </div>
+        <div class="input-group col-md-4" style="margin-left:2%; padding-top:2.5%;">
+          {{Form::submit('Enviar',array('class'=>'btn btn-primary'))}}
+        </div>
+      {{Form::close()}}
+    </div>
   </div>
 
   <button class="accordion">Discapacidad</button>
