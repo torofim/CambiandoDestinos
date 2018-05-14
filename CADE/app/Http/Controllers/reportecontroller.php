cdfe1f2ed4353c187f0771b0f8c06cedddaab7f5
Mauriciotorresnk <krothaloinf@gmail.com>
2018-05-14 13:21:18 -0600

    reporte

--- CADE/app/Http/Controllers/reportecontroller.php
+++ CADE/app/Http/Controllers/reportecontroller.php
@@ -9,6 +9,11 @@ class reportecontroller extends Controller
 {
     //
     public function index(){
-      return View('reporte');
+      $Id=1;
+      registros=\DB::table('datos_personales')
+      ->where('Id_bene','=',$Id)
+      ->orderby('Id_bene','desc');
+      return View('reporte')
+      ->with('Id_bene',$Id);
     }
 }
